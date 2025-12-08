0d0fb9ee045954f3ce445bb80d2fb6e80f54f5f4
ThiDaZ <wthidas95@gmail.com>
2025-12-08 22:10:52 +0530

    Added tmdb properties with base url and api config

--- src/main/java/com/thidas/tmdbgo/TmdbGoApplication.java
+++ src/main/java/com/thidas/tmdbgo/TmdbGoApplication.java
@@ -1,9 +1,13 @@
 package com.thidas.tmdbgo;
 
+import com.thidas.tmdbgo.client.TmdbProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(TmdbProperties.class)
 public class TmdbGoApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/thidas/tmdbgo/TmdbGoApplicationTests.java
+++ src/test/java/com/thidas/tmdbgo/TmdbGoApplicationTests.java
@@ -1,13 +1,24 @@
 package com.thidas.tmdbgo;
 
+import com.thidas.tmdbgo.client.TmdbProperties;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(
+)
 class TmdbGoApplicationTests {
 
+    @Autowired
+    private TmdbProperties tmdbProperties;
+
     @Test
-    void contextLoads() {
+    void shouldLoadConfigProperties() {
+
+        assertThat(tmdbProperties.getBaseUrl()).isEqualTo("https://api.themoviedb.org/3");
+        assertThat(tmdbProperties.getApiKey()).isNotBlank();
     }
 
 }
