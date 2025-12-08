6309523a303767d4919ec073c952c346885573ee
ThiDaZ <wthidas95@gmail.com>
2025-12-08 20:05:37 +0530

    first commit

---
+++ src/main/java/com/thidas/tmdbgo/TmdbGoApplication.java
@@ -0,0 +1,13 @@
+package com.thidas.tmdbgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmdbGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmdbGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thidas/tmdbgo/TmdbGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thidas.tmdbgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmdbGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
